4055ec5d336367bb43a054956a4903992248ac8e
jnt2811 <genetek.technical@gmail.com>
2023-04-14 16:28:05 +0700

    ......

---
+++ src/pages/liquidity-vaults.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from "next";
+import { Page, Section } from "~/components";
+
+const LiquidityVaults: NextPage = () => {
+  return (
+    <Page>
+      <Section>DAO</Section>
+    </Page>
+  );
+};
+
+export default LiquidityVaults;
